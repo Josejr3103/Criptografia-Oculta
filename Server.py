71bd7644f1781d17300141d69dc06f9435d9931e
José Roberto de Sousa Brito Júnior <81085769+Josejr3103@users.noreply.github.com>
2026-05-29 23:30:12 -0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import socket
+from Decript import decript_cesar
 
 SERVER_IP = '0.0.0.0' #Escuta em todas as interfaces de rede
 PORT = 5000
@@ -13,4 +14,13 @@ while True:
     print(f"Conexão aceita de {addr[0]}:{addr[1]}")
     data = client_socket.recv(1024).decode('utf-8')
     print(f"[!] Mensagem recebida(criptografada) : {data}")
+    
+    # Tentar todas as chaves de 0 a 100 mostrando apenas as mensagens que tem texto e são minúsculas (possíveis mensagens legíveis)
+    print("\n[*] Tentando descriptografar com chaves de 0 a 100:")
+    for chave in range(101):
+        mensagem_descriptografada = decript_cesar(data, chave)
+        if mensagem_descriptografada.isalpha() and mensagem_descriptografada.islower():
+            print(f"  Chave [{chave}]: {mensagem_descriptografada}")
+    
+    print()
     client_socket.close()
